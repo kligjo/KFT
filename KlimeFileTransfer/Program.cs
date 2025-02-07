ee738f206d9dfa7e92aba6b101d70d015b673186
Kliment <kligjo@gmail.com>
2025-02-07 16:03:13 +0100

    Add project files.

---
+++ KlimeFileTransfer/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace KFT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string sourcePath = GetPathsInput("Please enter your source file destination");
+            string destinationPath = GetPathsInput("Please enter your destination file destination");
+
+            FileStream sourceStream = new FileStream(destinationPath, FileMode.Open, FileAccess.Read);
+            FileStream destinationStream = new FileStream(destinationPath, FileMode.Create, FileAccess.Write);
+        }
+
+        static string GetPathsInput(string userMessage)
+        {
+            string path;
+
+            Console.WriteLine(userMessage);
+            path = Console.ReadLine().Trim();
+
+            return path;
+
+        }
+    }
+}
