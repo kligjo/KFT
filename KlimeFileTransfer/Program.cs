eb1c94b4ae058740ef25285afbf8956cc85378d0
Kliment <kligjo@gmail.com>
2025-02-09 20:02:57 +0100

    adding source hash and the checksum of each block

--- KlimeFileTransfer/Program.cs
+++ KlimeFileTransfer/Program.cs
@@ -1,23 +1,44 @@
-﻿namespace KFT
+﻿using System.Diagnostics;
+using System.Security.Cryptography;
+
+namespace KFT
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
 
             string sourcePath = GetPathsInput("Please enter your source file destination: ");
             string destinationPath = GetPathsInput("Please enter your destination file destination: ");
 
-            CopyFileToDestination(sourcePath, destinationPath);
+            await CopyFileToDestination(sourcePath, destinationPath);
 
         }
 
-        static void CopyFileToDestination(string source, string destination)
+        static async Task CopyFileToDestination(string source, string destination)
         {
-            FileStream sourceStream = new FileStream(source, FileMode.Open, FileAccess.Read);
-            FileStream destinationStream = new FileStream(destination, FileMode.Create, FileAccess.Write);
-
+            using FileStream sourceStream = new FileStream(source, FileMode.Open, FileAccess.Read);
+            using FileStream destinationStream = new FileStream(destination, FileMode.Create, FileAccess.Write);
+            using var md5 = MD5.Create();
+            byte[] buffer = new byte[1024*1024];
             
+            int bytesRead;
+            int position = 0;
+            string loadingMessage = "";
+
+            while ((bytesRead = sourceStream.Read(buffer)) > 0)
+            {
+                var sourceHash = md5.ComputeHash(buffer);
+                await destinationStream.WriteAsync(buffer, 0, bytesRead);
+
+                var tempBuffer = new byte[bytesRead];
+                Console.WriteLine($"Position {position} hash {BitConverter.ToString(sourceHash)}");
+                position += 1024;
+               
+
+            }
+
+
         }
 
         static string GetPathsInput(string userMessage)
