49f4f38a57b4317f1c3bece634489f8b131a9aa8
Kliment <kligjo@gmail.com>
2025-02-10 15:22:50 +0100

    adding fix for the last smaller sized chunk adding check if a chung is dirty

---
+++ KlimeFileTransfer/DirtChunkException.cs
@@ -0,0 +1,14 @@
+﻿
+namespace KFT
+{
+    internal class DirtChunkException : Exception
+    {
+        public DirtChunkException()
+        {
+        }
+
+        public DirtChunkException(string? message) : base(message)
+        {
+        }
+    }
+}
--- KlimeFileTransfer/Program.cs
+++ KlimeFileTransfer/Program.cs
@@ -7,16 +7,20 @@ namespace KFT
     {
         static async Task Main(string[] args)
         {
+            string sourcePath = "C:\\Users\\klime\\Downloads\\testFile.zip";// GetPathsInput("Please enter your source file destination: ");
+            string destinationPath = "C:\\Users\\klime\\Desktop\\testFile.zip";// GetPathsInput("Please enter your destination file destination: ");
 
-            string sourcePath = GetPathsInput("Please enter your source file destination: ");
-            string destinationPath = GetPathsInput("Please enter your destination file destination: ");
             try
             {
                 await CopyFileToDestination(sourcePath, destinationPath);
             }
-            catch (Exception ex)
+            catch (DirtChunkException e)
             {
-                Console.WriteLine("Greskaa");
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
             }
 
         }
@@ -27,25 +31,41 @@ namespace KFT
             using FileStream destinationStream = new FileStream(destination, FileMode.Create, FileAccess.ReadWrite);// adding read/write so that we can read the written bytes fromthe new location to hash them and check the hashes
             using var md5 = MD5.Create();
             byte[] buffer = new byte[1024 * 1024];
+            var actualBuffer = new byte[1024 * 1024];
 
             int bytesRead;
-            int position = 0;
+            bool isDirtyChunk = false;
 
             while ((bytesRead = sourceStream.Read(buffer)) > 0)
             {
+                if (bytesRead != buffer.Length)
+                {
+                    actualBuffer = new byte[bytesRead];
+                    Array.Copy(buffer, actualBuffer, bytesRead);
+                    buffer = actualBuffer;
+                }
+
                 var sourceHash = BitConverter.ToString(md5.ComputeHash(buffer));
 
                 await destinationStream.WriteAsync(buffer, 0, bytesRead);
 
+                var destinationHash = await ComputeDestionationHash(buffer.Length, destinationStream, md5);
 
-                var destinationHash = await ComputeDestionationHash(bytesRead, destinationStream, md5); ;
+                Console.WriteLine($"Position {destinationStream.Position / 1024} hash {sourceStream.Position / 1024}");
 
-                Console.WriteLine($"Position {position} hash {destinationHash}");
-                position += 1024;
 
+                if (!sourceHash.Equals(destinationHash))
+                {
+                    sourceStream.Position -= bytesRead;
+                    destinationStream.Position -= bytesRead;
+                    isDirtyChunk = true;
+                }
+
+                if (isDirtyChunk)
+                {
+                    throw new DirtChunkException("Dirty chunk found");
+                }
             }
-
-
         }
 
         static string GetPathsInput(string userMessage)
@@ -56,7 +76,6 @@ namespace KFT
             path = Console.ReadLine().Trim();
 
             return path;
-
         }
 
         static async Task<string> ComputeDestionationHash(int bytesRead, FileStream stream, MD5 md5)
